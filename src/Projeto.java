ced8e848871f745ceaa301da464d7582f1a848f7
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2020-01-14 15:40:05 -0300

    Testar funcionalidades do Git

---
+++ src/Projeto.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Projeto {
+    public static void main(String[] args) {
+
+        int esporte = 1;
+        switch (esporte){
+            case 1:
+                System.out.println("Basquetebol");
+                break;
+            case 2:
+                System.out.println("Futebol");
+                break;
+            case 3:
+                System.out.println("Voleibol");
+                break;
+            case 4:
+                System.out.println("Corrida");
+                break;
+            case 5:
+                System.out.println("Natação");
+                break;
+
+        }
+
+    }
+}
